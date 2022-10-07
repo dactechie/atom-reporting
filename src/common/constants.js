a7f6aa857405e37f5c39c64867998eb2f5a39242
dactechie <directionshealth.forms@gmail.com>
2022-10-08 09:59:08 +1300

    rolling back change to DB name

--- src/common/constants.js
+++ src/common/constants.js
@@ -10,8 +10,7 @@ export const MODE_EMPTY_CLIENT_DATA = 0;
 export const MODE_CLIENT_DATA_SET = 1; // show client survey history
 
 console.log("App environement", APP_ENVIRONMENT);
-export const DB_ENTITY_NAME =
-  APP_ENVIRONMENT === "staging" ? "StagingCOMS" : "COMS";
+export const DB_ENTITY_NAME = "COMS";
 
 export const DB_META_KEYS = [
   PARTITION_KEY,
